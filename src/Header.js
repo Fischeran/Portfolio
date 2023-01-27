eaba042310609185f41a5838ad4c5e65ad8c00c2
Fischer <alnfischer@gmail.com>
2023-01-26 22:34:21 -0500

    adding flex

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,25 @@
 import React from "react";
-
+import "./Header.css"
 
 
 function Header() {
 
     return (
-        <p>Header</p>
+        <div className="header row-md">
+           <nav className="nav row">
+                    <div>
+                        <p>Alex Fischer</p>
+                    </div>
+                    
+                    <div className="navBut">
+                        <a href="#" className="navBar">Home</a>
+                        <a href="#" className="navBar">About</a>
+                        <a href="#" className="navBar">Skills</a>
+                        <a href="#" className="navBar">Portfolio</a>
+                        <a href="#" className="navBar">Contact</a>
+                    </div>    
+            </nav>    
+        </div>
     )
 }
 
