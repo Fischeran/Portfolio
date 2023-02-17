c10a9a389ed20b1218c9355418c8fb07ca09bbeb
Fischer <alnfischer@gmail.com>
2023-02-16 23:05:30 -0500

    adding images and second section

--- src/GreenIntro.js
+++ src/GreenIntro.js
@@ -1,10 +1,14 @@
 import React from "react";
+import arrow from "./images/arrow-point-to-right.png"
+
 
 
 function GreenIntro() {
     return (
-        <div className="row-lg" style={{backgroundColor: "rgb(141, 223, 18)" , padding: "150px"}}>
-            <p>test</p>
+        <div className="row-lg mainSec" style={{backgroundColor: "rgb(141, 223, 18)" , padding: "150px"}}>
+            <p>Welcome!</p>
+            <a href="#">Let me show you around</a>
+            <img src={arrow} className="arrow" style={{maxWidth: "3%"}} />
         </div>
     )
 
