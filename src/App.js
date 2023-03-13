2ea5d0e5e4b5920e8fdc74864df9071979a003cf
Fischer <alnfischer@gmail.com>
2023-03-13 13:54:02 -0400

    adding in Projects section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./Header";
 import GreenIntro from "./GreenIntro";
 import About from "./About";
+import Projects from "./Projects";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
   <Header />
   <GreenIntro />
   <About />
+  <Projects />
   </>
   )
 }
---
+++ src/Projects.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+function Projects() {
+
+    return (
+        <section className="container-fluid ">
+            <div className="row">
+                <h2 className="projectHeader">Projects</h2>
+            </div>
+
+            <div className="row">
+                <div className="col">
+                    <h4>Periodic Tables: Resteraunt Reservation Web App</h4>
+                    <p>Full-Stack Web application that helps Resteraunt owners book and reservations and manage seating.
+                        <b>Built Using:</b>React, Node.JS, JavaScript, PostgreSQL, Express, HTML5, Bootstrap, CSS, Knex
+                    </p>
+                </div>
+            </div>
+
+        </section>
+    )
+
+
+}
+
+
+export default Projects
