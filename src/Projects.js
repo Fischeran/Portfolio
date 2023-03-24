99385ead6e6aff30bb06f30d60e8a89a81ba652a
Fischer <alnfischer@gmail.com>
2023-03-23 22:23:14 -0400

    adding screenshots of projects

--- src/Projects.js
+++ src/Projects.js
@@ -1,4 +1,5 @@
 import React from "react";
+import periodicTables from "./images/periodicTables.png"
 
 
 function Projects() {
@@ -9,12 +10,15 @@ function Projects() {
                 <h2 className="projectHeader">Projects</h2>
             </div>
 
-            <div className="row">
+            <div className="row periodD">
                 <div className="col">
                     <h4>Periodic Tables: Resteraunt Reservation Web App</h4>
                     <p>Full-Stack Web application that helps Resteraunt owners book and reservations and manage seating.
                         <b>Built Using:</b>React, Node.JS, JavaScript, PostgreSQL, Express, HTML5, Bootstrap, CSS, Knex
                     </p>
+                </div>
+                <div className="col">
+                    <img alt="Periodic Tables resteraunt reservation app" src={periodicTables} className="period" />
                 </div>
             </div>
 
